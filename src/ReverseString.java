c44ba27fd37e8fabe50f1a1d4f7c4725b7e21d65
am1988 <muzyka.andriy@gmail.com>
2018-03-26 12:05:44 +0300

    Reverse String problem solving

--- src/ReverseString.java
+++ src/ReverseString.java
@@ -1,8 +1,8 @@
 /**
  * Write a function that takes a string as input and returns the string reversed.
-
- Example:
- Given s = "hello", return "olleh".
+ * <p>
+ * Example:
+ * Given s = "hello", return "olleh".
  */
 
 public class ReverseString {
@@ -14,7 +14,18 @@ public class ReverseString {
 
     public static void main(String[] args) {
         ReverseString revInt = new ReverseString();
-        System.out.println(revInt.reverseString("hellpdffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff5656yy56u56hdrthrhtghjhellpdffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff5656yy56u56hdrthrhtghjhellpdffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff5656yy56u56hdrthrhtghjhellpdffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff5656yy56u56hdrthrhtghjhellpdffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff5656yy56u56hdrthrhtghjhellpdffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff5656yy56u56hdrthrhtghjhellpdffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff5656yy56u56hdrthrhtghjhellpdffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff5656yy56u56hdrthrhtghjhellpdffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff5656yy56u56hdrthrhtghjhellpdffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff5656yy56u56hdrthrhtghjhellpdffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff5656yy56u56hdrthrhtghj"));
+        System.out.println(revInt.reverseString("hellpdffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff" +
+                "ffffffffffffff5656yy56u56hdrthrhtghjhellpdffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff" +
+                "ffffffffffffff5656yy56u56hdrthrhtghjhellpdffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff" +
+                "ffffffffffffff5656yy56u56hdrthrhtghjhellpdffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff" +
+                "ffffffffffffff5656yy56u56hdrthrhtghjhellpdfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff" +
+                "fffffffffffff5656yy56u56hdrthrhtghjhellpdffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff" +
+                "ffffffffffff5656yy56u56hdrthrhtghjhellpdfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff" +
+                "fffffffffff5656yy56u56hdrthrhtghjhellpdfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff" +
+                "fffffffff5656yy56u56hdrthrhtghjhellpdffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff" +
+                "ffffff5656yy56u56hdrthrhtghjhellpdffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff" +
+                "ffff5656yy56u56hdrthrhtghjhellpdffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff" +
+                "ff5656yy56u56hdrthrhtghj"));
 
     }
 }
